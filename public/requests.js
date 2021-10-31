4fc6b188efd2975e7ac972088713ebe1b1bd831c
gabriel.amaral <gabriel.amaral@mundiale.com>
2021-10-31 16:41:39 -0300

    fix: responses status; feat: blink mode and code refactor

--- public/requests.js
+++ public/requests.js
@@ -1,55 +1,78 @@
+let interval = null;
 const turnLedOn = async () => {
-    const response = await fetch('http://localhost:3300/pinport/13/on', {
-        method: 'GET'
-    })
-    const elements = document.getElementsByClassName('led-red')
-    for(element of elements){
-        element.style.backgroundColor= '#F00';
-    }
+  if (interval) clearInterval(interval);
+  checkState();
+  const response = await fetch("http://localhost:3300/pinport/13/on", {
+    method: "GET",
+  });
+  const elements = document.getElementsByClassName("led-red");
+  for (element of elements) {
+    element.style.backgroundColor = "#F00";
+  }
 };
 
 const turnLedOff = async () => {
-    const response = await fetch('http://localhost:3300/pinport/13/off', {
-        method: 'GET'
-    })
-    const elements = document.getElementsByClassName('led-red')
-    for(element of elements){
-        element.style.backgroundColor= "#A00"
-    }
+  if (interval) clearInterval(interval);
+  checkState();
+  const response = await fetch("http://localhost:3300/pinport/13/off", {
+    method: "GET",
+  });
+  const elements = document.getElementsByClassName("led-red");
+  for (element of elements) {
+    element.style.backgroundColor = "#A00";
+  }
 };
 
 const toggleLed = async () => {
-    const response = await fetch('http://localhost:3300/pinport/13/toggle', {
-        method: 'GET'
-    })
-    const elements = document.getElementsByClassName('led-red')
-    for(element of elements){
-        const { pinState } = await response.json()
-        if(pinState){
-            element.style.backgroundColor= '#F00';
-        }
-        else {
-            element.style.backgroundColor= "#A00"
-        }
+  if (interval) clearInterval(interval);
+  checkState();
+  const response = await fetch("http://localhost:3300/pinport/13/toggle", {
+    method: "GET",
+  });
+  const elements = document.getElementsByClassName("led-red");
+  for (element of elements) {
+    const { pinState } = await response.json();
+    alternateColor(element, pinState);
+  }
+};
+
+const alternateColor = (element, pinState) => {
+  if (pinState) {
+    element.style.backgroundColor = "#F00";
+  } else {
+    element.style.backgroundColor = "#A00";
+  }
+};
+
+const setBlinking = async () => {
+  if (interval) clearInterval(interval);
+  const response = await fetch("http://localhost:3300/pinport/13/blink", {
+    method: "GET",
+  });
+  const elements = document.getElementsByClassName("led-red");
+  const { pinState } = await response.json();
+  let state = pinState;
+  time = 500;
+  interval = setInterval(() => {
+    state = !state;
+    for (element of elements) {
+      alternateColor(element, state);
+      console.log(state);
     }
+  }, time);
 };
 
 const checkState = async () => {
-    const response = await fetch('http://localhost:3300/pinport/13', {
-        method: 'GET'
-    })
-    const elements = document.getElementsByClassName('led-red')
-    for(element of elements){
-        const { pinState } = await response.json()
-        if(pinState){
-            element.style.backgroundColor= '#F00';
-        }
-        else {
-            element.style.backgroundColor= "#A00"
-        }
-    }
-}
+  const response = await fetch("http://localhost:3300/pinport/13", {
+    method: "GET",
+  });
+  const elements = document.getElementsByClassName("led-red");
+  for (element of elements) {
+    const { pinState } = await response.json();
+    alternateColor(element, pinState);
+  }
+};
 
-window.addEventListener('load', ()=>{
-    checkState()
-})
+window.addEventListener("load", () => {
+  checkState();
+});
--- src/controller/arduino-controller.js
+++ src/controller/arduino-controller.js
@@ -1,85 +1,103 @@
-const log4js = require('log4js')
+const log4js = require("log4js");
 const logConfig = require("../configs/log4js.json");
 log4js.configure(logConfig);
 const logger = log4js.getLogger();
 
-const board = require('../models/arduino')
+const board = require("../models/arduino");
 
 class LedController {
+  constructor() {
+    this.pins = process.env.ARDUINO_Pins || [];
+    this.pinsState = {};
+    this.pinMode = ["on", "off", "toggle", "blink"];
+    this.lastInstructions = {};
+    this.instruction = {
+      on: (pin) => {
+        this.pinsState[pin] = 1;
+        board.digitalWrite(pin, 1);
+        this.setLastInstructions(pin, "on");
+        return { pin, pinState: this.pinsState[pin] };
+      },
+      off: (pin) => {
+        this.pinsState[pin] = 0;
+        board.digitalWrite(pin, 0);
+        this.setLastInstructions(pin, "off");
+        return { pin, pinState: this.pinsState[pin] };
+      },
+      toggle: (pin) => {
+        this.pinsState[pin] = this.pinsState[pin] ? 0 : 1;
+        board.digitalWrite(pin, this.pinsState[pin]);
+        this.setLastInstructions(pin, "toggle");
+        return { pin, pinState: this.pinsState[pin] };
+      },
+      blink: (pin, time = 500) => {
+        this.pinsState[pin] = this.pinsState[pin] ? 0 : 1;
+        this.setLastInstructions(pin, "blink");
+        board.loop(time, (stop) => {
+          this.pinsState[pin] = !this.pinsState[pin];
+          board.digitalWrite(pin, this.pinsState[pin]);
+          if (
+            this.lastInstructions[pin].mode !== "blink" ||
+            this.lastInstructions[pin].calledTimes > 1
+          ) {
+            stop();
+            this.lastInstructions[pin].calledTimes = 1;
+          }
+        });
+        return { pin, pinState: this.pinsState[pin] };
+      },
+    };
+  }
 
-    constructor(){
-        this.pins = process.env.ARDUINO_Pins || []
-        this.pinsState= {}
-        this.pinMode = ['on', 'off', 'toggle']
+  setLastInstructions(pin, instruction) {
+    if (
+      this.lastInstructions[pin] &&
+      this.lastInstructions[pin]?.mode === instruction
+    ) {
+      this.lastInstructions[pin].calledTimes += 1;
+      return true;
     }
+    this.lastInstructions[pin] = { mode: instruction, calledTimes: 1 };
+  }
 
-    proccess(req, res, next) {
-        //logger.info(this)
-        try{
-            const { mode, pin } = req.params
-            const Pin = String(pin)
-            const physicalPorts = {'1':'1'}
-            if(this.pins.includes(Pin)){
-                switch(mode.toLowerCase()){
-                    case 'toggle':
-                        this.pinsState[Pin] = !this.pinsState[Pin]
-                        board.digitalWrite(Pin, this.pinsState[Pin] ? 1 : 0)
-                        res.json({
-                            pin,
-                            pinState: this.pinsState[Pin],
-                            message:this.pinsState[Pin] ? 'Pin ON!' : 'Led OFF!'
-                        })
-                        break;
-                    case 'on':
-                        this.pinsState[Pin] = 1
-                        board.digitalWrite(Pin, 1)
-                        res.json({
-                            pin,
-                            pinState: this.pinsState[Pin],
-                            message:'Pin ON!'
-                        })
-                        break;
-                    case 'off':
-                        this.pinsState[Pin] = 0
-                        board.digitalWrite(Pin, 0)
-                        res.json({
-                            pin,
-                            pinState: this.pinsState[Pin],
-                            message:'Pin OFF!'
-                        })
-                        break;
-                    default:
-                        res.json({
-                            errorMessage:'Invalid mode!'
-                        })
-                }
-            } else {
-                res.json({ 
-                    errorMessage: 'Invalid Pin or not initialized!'
-                })
-            }
-            
-            
-        } catch(error) {
-            console.log(error)
-            res.json({
-                errorMessage:'Something went wrong!'
-            })
+  proccess(req, res, next) {
+    //logger.info(this)
+    try {
+      const { mode, pin, time } = req.params;
+      const Pin = String(pin);
+      if (this.pins.includes(Pin)) {
+        if (this.pinMode.includes(mode)) {
+          const result = this.instruction[mode](Pin, time);
+          res.status(200).json({
+            ...result,
+            message: this.pinsState[Pin] ? "Pin ON!" : "Led OFF!",
+          });
+        } else {
+          res.status(400).json({ error: "Invalid mode" });
         }
+      } else {
+        res.status(400).json({ error: "Invalid pin" });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        errorMessage: error.message || "Something went wrong!",
+      });
     }
+  }
 
-    getState(req, res, next){
-        const { pin } = req.params
-        const Pin = String(pin)
+  async getState(req, res, next) {
+    const { pin } = req.params;
+    const Pin = String(pin);
 
-        if(this.pins.includes(Pin)){
-            res.json({
-                pin,
-                pinState: this.pinsState[Pin] || 0,
-                message: this.pinsState[Pin] ? 'Pin ON!' : 'Pin OFF!'
-            })        
-        }
+    if (this.pins.includes(Pin)) {
+      res.json({
+        pin,
+        pinState: this.pinsState[Pin] || 0,
+        message: this.pinsState[Pin] ? "Pin ON!" : "Pin OFF!",
+      });
     }
+  }
 }
 
 module.exports = LedController;
